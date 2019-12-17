d01c32b92a4b2b8a63d7e1fa0f7b04610559f883
FireDev <33162551+iiFir3z@users.noreply.github.com>
2019-12-17 12:10:27 -0800

    Added loading util for debugging purp.

---
+++ main.py
@@ -0,0 +1,17 @@
+# Allen can't speak proper english edition.
+# Now with more autism!
+# and really bad story line
+import os
+import time
+import subprocess
+# Check version for updates. If updates are present they will install here.
+print("Grabbing version info...")
+time.sleep(2)
+print("Installing latest build of Zoink. (FireUpdater v1)")
+os.system("wget -O game.py https://raw.githubusercontent.com/iiFir3z/zoink/master/game.py")
+print("Update successfully installed. Starting application.")
+time.sleep(4)
+os.system("clear")
+# Starting application.
+os.system("python game.py")
+
