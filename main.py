9dee3ab4a76f707cf4da9e97f03d6f247951f59b
FireDev <33162551+iiFir3z@users.noreply.github.com>
2019-12-19 07:53:09 -0800

    Trying python version of wget.
    May succeed in the case that it doesn't I will be writing a custom module.

--- main.py
+++ main.py
@@ -3,26 +3,30 @@
 # and really bad story line
 import os
 import time
+import wget
 # Check version for updates. If updates are present they will install here.
 print("FireUpdater v3")
 print("Grabbing version info...")
-os.system("wget -O latest.txt https://raw.githubusercontent.com/iiFir3z/zoink/master/latest.txt")
+latest = "https://raw.githubusercontent.com/iiFir3z/zoink/master/latest.txt"
+wget.download(latest)
 currentopen = open("ver.txt", "r")
 currentver = currentopen.read()
 latestopen = open("latest.txt", "r")
 latestver = latestopen.read()
 if currentver == latestver :
   print("Up to date! Continuing with program.")
-  os.system("clear")
+  os.system("cls")
   os.system("python game.py")
 else:
   print("Version is out of date!")
-  os.system("wget -O ver.txt https://raw.githubusercontent.com/iiFir3z/zoink/master/ver.txt")
+  verfile = "https://raw.githubusercontent.com/iiFir3z/zoink/master/ver.txt"
+  wget.download(verfile)
   time.sleep(2)
   print("Installing latest build of Zoink. (FireUpdater v3)")
-  os.system("wget -O game.py https://raw.githubusercontent.com/iiFir3z/zoink/master/game.py")
+  gamedl = "https://raw.githubusercontent.com/iiFir3z/zoink/master/game.py"
+  wget.download(gamedl)
   print("Update successfully installed. Starting application.")
   time.sleep(4)
-  os.system("clear")
+  os.system("cls")
   # Starting application.
   os.system("python game.py")
