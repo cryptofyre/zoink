dfd451341ad892fc1453ce1030cd436101257f33
FireDev <33162551+iiFir3z@users.noreply.github.com>
2019-12-19 08:06:08 -0800

    Updated the updater to work with windows!

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import wget
 # Check version for updates. If updates are present they will install here.
 print("FireUpdater v3")
 print("Grabbing version info...")
-latest = "https://raw.githubusercontent.com/iiFir3z/zoink/master/latest.txt"
-wget.download(latest)
+os.system("python -m wget -O latest.txt https://raw.githubusercontent.com/iiFir3z/zoink/windows/latest.txt")
 currentopen = open("ver.txt", "r")
 currentver = currentopen.read()
 latestopen = open("latest.txt", "r")
@@ -19,12 +18,10 @@ if currentver == latestver :
   os.system("python game.py")
 else:
   print("Version is out of date!")
-  verfile = "https://raw.githubusercontent.com/iiFir3z/zoink/master/ver.txt"
-  wget.download(verfile)
+  os.system("python -m wget -O ver.txt https://raw.githubusercontent.com/iiFir3z/zoink/windows/ver.txt")
   time.sleep(2)
   print("Installing latest build of Zoink. (FireUpdater v3)")
-  gamedl = "https://raw.githubusercontent.com/iiFir3z/zoink/master/game.py"
-  wget.download(gamedl)
+  os.system("python -m wget -O game.py https://raw.githubusercontent.com/iiFir3z/zoink/windows/game.py")
   print("Update successfully installed. Starting application.")
   time.sleep(4)
   os.system("cls")
